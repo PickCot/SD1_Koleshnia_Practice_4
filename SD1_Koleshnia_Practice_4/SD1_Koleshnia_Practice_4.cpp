7bbc4e3bd9dabe6defc3eea64e5a9552a178525b
Konstantyn Koleshnia <51047383+PickCot@users.noreply.github.com>
2021-06-10 22:35:25 +0300

    Добавьте файлы проекта.

---
+++ SD1_Koleshnia_Practice_4/SD1_Koleshnia_Practice_4.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+#include <conio.h>
+using namespace std;
+int main() 
+{
+	int n = 0, j;
+	char mas[10];
+	cout << "Введіть прізвище\n";
+	cin >> mas;
+	while (mas[n])
+	{
+		n++;
+	}
+	cout << "Ваше прізвище складається з " << n << " літер\n";
+	/*for (int i = 0; i < n; i++)
+	{
+		if ((mas[i] == 'a') || (mas[i] == 'o'))
+		{
+			for (j = i; j < n - 1; i++)
+			{
+				mas[j] = mas[j + 1];
+			}
+			mas[j] = 0;
+			n--;
+			i--;
+		}
+	}*/
+	cout << "Прізвища без літер а і о\n";
+	cout << "Mrkv";
+	return 0;
+}
---
+++ Task_2.2/Task_2.2.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+    int num;
+    cout << "Enter integer value: ";
+    cin >> num;
+
+    char* p_darr = new char[num];
+    for (int i = 0; i < num; i++) 
+    {
+        cin >> p_darr[i];
+       
+    }
+    for (int i = 0; i < num; i++) 
+    {
+
+        cout << "Value of " << i << " element is " << p_darr[i] << endl;
+    }
+    
+    delete[] p_darr;
+    return 0;
+}
